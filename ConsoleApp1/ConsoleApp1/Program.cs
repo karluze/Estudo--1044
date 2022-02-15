0eee2359272c771a6712858e01e8aaff57311d77
karluze <karluzem@gmail.com>
2022-02-15 11:35:07 -0300

    Projeto 1044

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int algarismoA, algarismoB;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            algarismoA = int.Parse(valores[0]);
+            algarismoB = int.Parse(valores[1]);
+
+            if (algarismoA % algarismoB == 0 || algarismoB % algarismoA == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+            //Console.ReadLine();
+        }
+    }
+}
